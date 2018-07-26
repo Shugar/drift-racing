e1673f422040a46f5ff45820ec92c934683f194a
Tigran Tumasov <tumasov@MacBook-Pro-Tigran.local>
2018-07-26 17:34:48 +0300

    generate

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,14 +1,16 @@
 const contentful = require('contentful')
+
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
+
 const config = {
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+  space: "u70qj7y74l9n",
+  accessToken: "c7e12517bd4ee9cdedb83b5c5131fb8c4321ac1c10ebca2c46bcd4bebe1785c6"
 }
 
 // export `createClient` to use it in page components
 module.exports = {
-  createClient() {
+  createClient () {
     return contentful.createClient(config)
   }
 }
