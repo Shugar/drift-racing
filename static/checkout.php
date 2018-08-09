94137ab41bc14d9cec9cec561687ec814ff18075
Tigran Tumasov <tumasov@MacBook-Pro-Tigran.local>
2018-08-09 17:32:27 +0300

    test checkout

--- static/checkout.php
+++ static/checkout.php
@@ -9,11 +9,15 @@ $address = $requestPayload['address'];
 $zip = $requestPayload['zip'];
 $products = json_decode($requestPayload['products']);
 
-$productListing = array_reduce($requestPayload['products'], function($listing, $product) {
-  return "${listing}\n{$product['title']}: {$product['price']}";
-}, "Ваш заказ: ");
+// $productListing = return "${listing}\n{$product['title']}: {$product['price']}";
+// $requestPayload['products']
+$data = '';
+foreach ($array as $key=>$value){
+      $data .= $key.'-------'.$value;
+      $data.= "\n";
+}
 
-if (mail("valeraerohin97@mail.ru", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $productListing"))
+if (mail("valeraerohin97@mail.ru", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $data"))
       echo is_string($productListing);
 else
       print("Не отправлено\r\n");
