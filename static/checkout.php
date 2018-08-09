b43e0ea5c6411ee7d9680eb3e6471076c8486bf2
vanilla-ice <valeraerohin97@mail.ru>
2018-08-09 14:17:40 +0300

    WIP: new php

--- static/checkout.php
+++ static/checkout.php
@@ -1,8 +1,5 @@
-<?php
-// Файлы phpmailer
-require 'class.phpmailer.php';
-require 'class.smtp.php';
-// Переменные
+
+<?
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
@@ -10,34 +7,11 @@ $address = $_POST['address'];
 $zip = $_POST['zip'];
 $products = json_decode($_POST['products']);
 
-foreach($products as $product) {
+print("test\r\n");
 
- echo $product;
+if (mail("valeraerohin97@mail.ru", "заголовок", "Имя $name . Телефон $phone . Почта $email Адрес $address . ZIP $zip . Products $products"))
+      print("Отправлено\r\n");
+else
+      print("Не отправлено\r\n");
 
-}
-
-// Настройки
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->Host = 'smtp.yandex.ru';
-$mail->SMTPAuth = true;
-$mail->Username = 'alexdracing'; // Ваш логин в Яндексе. Именно логин, без @yandex.ru
-$mail->Password = 'racing'; // Ваш пароль
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 587;
-$mail->setFrom('alexdracing@yandex.ru'); // Ваш Email
-$mail->addAddress('valeraerohin97@mail.ru'); // Email получателя
-
-// Письмо
-$mail->isHTML(true);
-$mail->Subject = "Заголовок"; // Заголовок письма
-$mail->Body = "Имя $name . Телефон $phone . Почта $email Адрес $address . ZIP $zip . products $products ."; // Текст письма
-
-// Результат
-if(!$mail->send()) {
-  echo 'Message could not be sent.';
-  echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-  echo 'ok';
-}
 ?>
