fe311aa1b151218fa99222a71b26c0fcccd23aaa
Tigran Tumasov <tumasov@MacBook-Pro-Tigran.local>
2018-08-09 18:20:25 +0300

    kek 1

--- static/checkout.php
+++ static/checkout.php
@@ -13,8 +13,8 @@ $productListing = array_reduce($requestPayload['products'], function($listing, $
   return "${listing}\n{$product['title']}: {$product['price']}";
 }, "Ваш заказ: ");
 
-if (mail("shugar348@gmail.com", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $productListing"))
-      echo is_string($productListing);
+if (mail("shugar348@gmail.com", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $products[0]['title']"))
+      echo $products[0]['title'];
 else
       print("Не отправлено\r\n");
 
