a0be904254786534a4bbe132f12d725ca7d6a861
vanilla-ice <valeraerohin97@mail.ru>
2018-08-09 14:42:21 +0300

    WIP: send post to email

--- static/checkout.php
+++ static/checkout.php
@@ -9,7 +9,7 @@ $address = $_POST['address'];
 $zip = $_POST['zip'];
 $products = json_decode($_POST['products']);
 
-if (mail("valeraerohin97@mail.ru", "заголовок", "Имя $name . Телефон $phone . Почта $email . Адрес $address . ZIP $zip . Products $products ."))
+if (mail("valeraerohin97@mail.ru", "заголовок", "$_POST"))
       echo json_encode($_POST);
 else
       print("Не отправлено\r\n");
