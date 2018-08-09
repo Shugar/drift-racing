21169a91a1f0d61015036322a9e61b87cb1230ae
vanilla-ice <valeraerohin97@mail.ru>
2018-08-09 14:45:54 +0300

    WIP: check products

--- static/checkout.php
+++ static/checkout.php
@@ -7,10 +7,10 @@ $phone = $_POST['phone'];
 $email = $_POST['email'];
 $address = $_POST['address'];
 $zip = $_POST['zip'];
-$products = json_decode($_POST['products']);
+$products = $_POST['products[]'];
 
-if (mail("valeraerohin97@mail.ru", "заголовок", "$_POST"))
-      echo json_encode($_POST);
+if (mail("valeraerohin97@mail.ru", "заголовок", "Имя $name . Телефон $phone . Почта $email . Адрес $address . ZIP $zip . Products $products ."))
+      echo $products;
 else
       print("Не отправлено\r\n");
 
