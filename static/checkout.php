b6203e77af18a06fe11d5fe6da341a24d6978370
Tigran Tumasov <tumasov@MacBook-Pro-Tigran.local>
2018-08-09 19:03:22 +0300

    lel kek pek q

--- static/checkout.php
+++ static/checkout.php
@@ -9,14 +9,9 @@ $address = $requestPayload['address'];
 $zip = $requestPayload['zip'];
 $products = $requestPayload['products'];
 
-// $productListing = array_reduce($requestPayload['products'], function($listing, $product) {
-//       return "${listing}\n{$product['title']}: {$product['price']}";
-// }, "Ваш заказ: ");
-
 $productListing = "";
 foreach ($products as $key => $value){
-      $productListing .= $key.'-------'.$value;
-      $productListing .= "\n";
+      $productListing .= $value['title'];
 }
 
 if (mail("sasu11@yandex.ru", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing"))
