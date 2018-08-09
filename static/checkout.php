0a09061a4737c75062efb9480e091001437d9204
Tigran Tumasov <tumasov@MacBook-Pro-Tigran.local>
2018-08-09 18:59:40 +0300

    lel kek pek

--- static/checkout.php
+++ static/checkout.php
@@ -9,13 +9,18 @@ $address = $requestPayload['address'];
 $zip = $requestPayload['zip'];
 $products = $requestPayload['products'];
 
-$productListing = array_reduce($requestPayload['products'], function($listing, $product) {
-      return "${listing}\n{$product['title']}: {$product['price']}";
-}, "Ваш заказ: ");
+// $productListing = array_reduce($requestPayload['products'], function($listing, $product) {
+//       return "${listing}\n{$product['title']}: {$product['price']}";
+// }, "Ваш заказ: ");
+
+$productListing = "";
+foreach ($products as $key => $value){
+      $productListing .= $key.'-------'.$value;
+      $productListing .= "\n";
+}
 
 if (mail("sasu11@yandex.ru", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing"))
-      // echo $products;
-      echo $products([0]);
+      echo $productListing;
 else
       print("Не отправлен");
 
