d4eb02806687055b6ba68a2b53e6bc9b953561ed
vanilla-ice <valeraerohin97@mail.ru>
2018-08-09 14:39:41 +0300

    WIP: fix $_POST

--- static/checkout.php
+++ static/checkout.php
@@ -1,5 +1,7 @@
 
 <?
+$_POST = json_decode(file_get_contents('php://input'), true);
+
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
@@ -8,7 +10,7 @@ $zip = $_POST['zip'];
 $products = json_decode($_POST['products']);
 
 if (mail("valeraerohin97@mail.ru", "заголовок", "Имя $name . Телефон $phone . Почта $email . Адрес $address . ZIP $zip . Products $products ."))
-      print($_POST);
+      echo json_encode($_POST);
 else
       print("Не отправлено\r\n");
 
