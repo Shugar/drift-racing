d253b534606e0fa2b719dd33c4cda105881ced96
Tigran Tumasov <tumasov@MacBook-Pro-Tigran.local>
2018-08-09 18:26:14 +0300

    kek

--- static/checkout.php
+++ static/checkout.php
@@ -7,15 +7,15 @@ $phone = $requestPayload['phone'];
 $email = $requestPayload['email'];
 $address = $requestPayload['address'];
 $zip = $requestPayload['zip'];
-$products = json_decode($requestPayload['products']);
+$products = $requestPayload['products'];
 
 $productListing = array_reduce($requestPayload['products'], function($listing, $product) {
-  return "${listing}\n{$product['title']}: {$product['price']}";
+      return "${listing}\n{$product['title']}: {$product['price']}";
 }, "Ваш заказ: ");
 
-if (mail("shugar348@gmail.com", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $products[0]['title']"))
+if (mail("shugar348@gmail.com", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing"))
       echo $products[0]['title'];
 else
-      print("Не отправлено\r\n");
+      print("Не отправлен");
 
 ?>
