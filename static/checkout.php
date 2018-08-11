6ef2137c4e305de62e05d3d64303c9af1ee75633
Tigran Tumasov <tumasov@MacBook-Pro-Tigran.local>
2018-08-11 03:36:12 +0300

    is it last fix?

--- static/checkout.php
+++ static/checkout.php
@@ -10,10 +10,13 @@ $zip = $requestPayload['zip'];
 $products = str_split($requestPayload['products']);
 
 $productListing = "";
-foreach ($products as $key => $value){
-      $productListing .= $products[$key]["title"];
-}
+// foreach ($products as $key => $value){
+//       $productListing .= $products[$key]["title"];
+// }
 
+foreach ($products as $value) {
+      $productListing .= $value["title"] + '\n';
+}
 
 if (mail("sasu11@yandex.ru", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $productListing"))
       echo $productListing;
