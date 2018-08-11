5339de280beb53d259bb4f7c27f9c7593bfcdab9
Ruslan <chunkey95@gmail.com>
2018-08-11 19:05:36 +0300

    saaaaaa

--- static/checkout.php
+++ static/checkout.php
@@ -15,9 +15,9 @@ foreach ($products as $value){
       $productListing .= $value["title"];
 }
 
-foreach ($products as $value) {
-      $productListing = $value;
-}
+// foreach ($products as $value) {
+//       $productListing = $value;
+// }
 
 if (mail("sasu11@yandex.ru", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $productListing"))
       echo $productListing;
