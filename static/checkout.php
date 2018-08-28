4d18e1db097aae4b9635342c9fa2920d303c1666
Dima Gudkov <d.o.gudkov@gmail.com>
2018-08-28 16:15:37 +0300

    edit checkout.php

--- static/checkout.php
+++ static/checkout.php
@@ -1,28 +1,50 @@
-
 <?
-$requestPayload = json_decode(file_get_contents('php://input'), true);
+$_request = json_decode(file_get_contents('php://input'), true);
 
-$name = $requestPayload['name'];
-$phone = $requestPayload['phone'];
-$email = $requestPayload['email'];
-$address = $requestPayload['address'];
-$zip = $requestPayload['zip'];
-// $products = str_split($requestPayload['products']);
-$products = $requestPayload['products'];
+list(
+  $name,
+  $email,
+  $address,
+  $zip,
+  $phone,
+  $products
+) = array_values($_request);
 
-$productListing = "";
-foreach ($products as $value){
-      $productListing .= $value["title"];
+$body = "<h2>Контактные данные</h2>
+         <p><b>Имя</b> {$name} </p>
+         <p><b>Телефон</b>: {$phone}</p>
+         <p><b>Почта</b>: {$email}</p>
+         <p><b>Адрес</b>: {$address}</p>
+         <p><b>Индекс</b>: {$zip}</p>
+         <br/><br/><hr/>";
+
+$_products = json_decode($products, true);
+if(count($_products))
+      $body .= "<h2>Информация о товарах</h2>";
+      
+foreach ($_products as $i => $product) {
+      list($type, $loc, $title, $gender, $category, $style, $price) = array_values($product);
+      $pos = $i + 1;
+      $body .= "<p><b>Позиция</b>: #{$pos}</p>";
+      $body .= "<p><b>Название</b>: {$title}</p>";
+      $body .= "<p><b>Цена</b>: {$price}</p>";
 }
 
-// foreach ($products as $value) {
-//       $productListing = $value;
-// }
+$timestamp = date("d.m.Y H:i:s", strtotime("now"));
+$to = "sasu11@yandex.ru";
+$subject = "Новая заявка из магазина от {$timestamp}";
+$headers = "From: noreply@alexdracing.com\r\n";
+$headers .= "Reply-To: noreply@alexdracing.com\r\n";
+$headers .= "CC: one.more.1@yandex.ru\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-if (mail("sasu11@yandex.ru", "заголовок", "Имя $name \r\n Телефон $phone \r\n Почта $email \r\n Адрес $address \r\n ZIP $zip \r\n Product Listing $productListing"))
-      // echo $productListing;
-      echo $products;
+
+if (mail($to, $subject, $body, $headers))
+      echo json_encode(["response" => "ok"]);
 else
-      print("Не отправлен");
+      echo json_encode(["response" => "error"]);
+
+exit;
 
 ?>
